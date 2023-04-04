8427e6826f4db451c33f2cf962e27c82b4512db5
michal-wojcik <6065832+michal-wojcik@users.noreply.github.com>
2023-04-04 20:07:40 +1000

    Add maven project

---
+++ src/test/java/au/michalwojcik/messaging/MessageHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package au.michalwojcik.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
